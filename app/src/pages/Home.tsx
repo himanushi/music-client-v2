3153e203fcca4a3f2d69caf75861a8d69f97652d
himanushi <himanushi@users.noreply.github.com>
2020-12-28 17:08:05 +0900

    ionic チュートリアル1

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,23 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBadge, IonCheckbox, IonContent, IonItem, IonLabel, IonList, IonNote, IonPage } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonList>
+      <IonItem>
+        <IonCheckbox slot="start" />
+        <IonLabel>
+          <h1>Create Idea</h1>
+          <IonNote>Run Idea by Brandy</IonNote>
+        </IonLabel>
+        <IonBadge color="success" slot="end">
+          5 Days
+        </IonBadge>
+      </IonItem>
+    </IonList>
       </IonContent>
     </IonPage>
   );
