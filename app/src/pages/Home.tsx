5a99950c8eb331b60a6138751c8b0b7c290db750
himanushi <himanushi@users.noreply.github.com>
2020-12-29 17:40:53 +0900

    prettier と eslint を追加

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,22 +1,31 @@
-import { IonBadge, IonCheckbox, IonContent, IonItem, IonLabel, IonList, IonNote, IonPage } from '@ionic/react';
-import React from 'react';
+import {
+  IonBadge,
+  IonCheckbox,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonNote,
+  IonPage,
+} from "@ionic/react";
+import React from "react";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-      <IonList>
-      <IonItem>
-        <IonCheckbox slot="start" />
-        <IonLabel>
-          <h1>Create Idea</h1>
-          <IonNote>Run Idea by Brandy</IonNote>
-        </IonLabel>
-        <IonBadge color="success" slot="end">
-          5 Days
-        </IonBadge>
-      </IonItem>
-    </IonList>
+        <IonList>
+          <IonItem>
+            <IonCheckbox slot="start" />
+            <IonLabel>
+              <h1>Create Idea</h1>
+              <IonNote>Run Idea by Brandy</IonNote>
+            </IonLabel>
+            <IonBadge color="success" slot="end">
+              5 Days
+            </IonBadge>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
