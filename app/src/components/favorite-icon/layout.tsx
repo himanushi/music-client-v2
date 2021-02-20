c4c1558c50d00ac99035f068dcf2c785019dd2e0
himanushi <himanushi@users.noreply.github.com>
2021-02-20 21:51:15 +0900

    お気に入り強化

--- app/src/components/favorite-icon/layout.tsx
+++ app/src/components/favorite-icon/layout.tsx
@@ -3,9 +3,27 @@ import Slot from "components/slot";
 import { star, starOutline } from "ionicons/icons";
 import React from "react";
 
-export const Favorite = ({ id }: { id: string }) => (
-  <IonIcon style={{ fontSize: 25 }} color="favorite" icon={star} />
-);
+export const Favorite = ({ id }: { id: string }) => {
+  const fontSize = 26;
+  const itemFontSize = fontSize + 2;
+
+  return (
+    <Slot
+      layout={<IonIcon style={{ fontSize }} color="warning" icon={star} />}
+      item={
+        <IonIcon
+          style={{ "--ionicon-stroke-width": "20px", fontSize: itemFontSize }}
+          color="black"
+          icon={starOutline}
+        />
+      }
+      center={true}
+      left={-1}
+      itemWidth={itemFontSize}
+      itemHeight={itemFontSize}
+    />
+  );
+};
 
 export const FavoriteOutline = () => {
   const fontSize = 26;
@@ -14,7 +32,6 @@ export const FavoriteOutline = () => {
     <Slot
       layout={
         <IonIcon
-          className="ion-no-padding"
           style={{ "--ionicon-stroke-width": "60px", fontSize }}
           color="medium"
           icon={starOutline}
@@ -22,7 +39,6 @@ export const FavoriteOutline = () => {
       }
       item={
         <IonIcon
-          className="ion-no-padding"
           style={{ "--ionicon-stroke-width": "20px", fontSize }}
           color="white"
           icon={starOutline}
